87e40ea080e73c3afb6d353ec0c749ebd7206d27
Aymon2016 <mdaiman2016@gmail.com>
2023-03-16 23:30:24 +0600

    deploy commit error solve

--- pages/about/index.js
+++ pages/about/index.js
@@ -4,22 +4,7 @@ const About = () => {
     return (
         <>
             <Navbar />
-            <div style={{
-                margin: '10px'
-            }}>
-                <h1 style={{ fontSize: '20px' }}>Phone Store Ltd</h1>
-                <h1 style={{ fontSize: '20px' }}>About Phone Store</h1>
-                <p style={{ fontSize: '15px', lineHeight: '20px' }}>
-                    Star Tech has always managed the standards for Quality management. In 2022, Star Tech Ltd. was certified with the well-known . This marked a groundbreaking achievement for us. As anorganization; we consistently maintain all sorts of regulatory requirements to provide the best products and services to meet any customer requirement.
-                </p>
-                <h1>ISO Certified Quality Management System</h1>
-                <p>Star Tech has always managed the standards for Quality management. In 2022, Star Tech Ltd. was certified with the well-known . This marked a groundbreaking achievement for us. As anorganization; we consistently maintain all sorts of regulatory requirements to provide the best products and services to meet any customer requirement.</p>
-                <h1>The Main Goal and Aim</h1>
-                <p>Star Tech has always managed the standards for Quality management. In 2022, Star Tech Ltd. was certified with the well-known . This marked a groundbreaking achievement for us. As anorganization; we consistently maintain all sorts of regulatory requirements to provide the best products and services to meet any customer requirement.</p>
-
-                <h1>Top-Selling Brands</h1>
-                <p>Star Tech has always managed the standards for Quality management. In 2022, Star Tech Ltd. was certified with the well-known . This marked a groundbreaking achievement for us. As anorganization; we consistently maintain all sorts of regulatory requirements to provide the best products and services to meet any customer requirement.</p>
-            </div>
+            <h1>hello about page</h1>
         </>
     )
 }
